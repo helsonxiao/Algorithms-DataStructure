a83ded4e153853615422b35c8de76108dc8575aa
helsonxiao <helsonxiao@gmail.com>
2018-02-12 19:01:03 +0800

    modify head node and params

--- Node/GetElem.c
+++ Node/GetElem.c
@@ -5,7 +5,7 @@ Status GetElem (LinkList L, int i , ElemType *e)
 {
     int j = 1;
     LinkList p;
-    p = L->next;
+    p = L->next; // first node
     while (p && j<i)
     {
         p = p->next;
@@ -23,15 +23,16 @@ int main()
 {
     LinkList list;
     Node n0, n1, n2;
-    n0.data = 0;
+    list = &n0;
     n0.next = &n1;
+
     n1.data = 1;
     n1.next = &n2;
     n2.data = 2;
     n2.next = NULL;
-    list = &n0;
 
-    int e;
-    int t = GetElem(list, 2, &e);
-    printf("%d\n%d", t, e);
+    ElemType e;
+    int index = 2;
+    GetElem(list, index, &e);
+    printf("%d", e);
 }
