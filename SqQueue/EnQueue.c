cb1618fd32d6a16aafacd7f2e28c6feea4105b94
helsonxiao <helsonxiao@gmail.com>
2018-04-20 16:36:17 +0800

    move builtin methods

--- SqQueue/EnQueue.c
+++ SqQueue/EnQueue.c
@@ -1,30 +1,5 @@
 #include "SqQueue.c"
 
-Status EnQueue(SqQueue *Q, QElemType e)
-{
-    if ((Q->rear + 1) % MAXSIZE == Q->front)
-    {
-        return ERROR;
-    }
-
-    Q->data[Q->rear] = e;
-    Q->rear = (Q->rear + 1) % MAXSIZE;
-    return OK;
-}
-
-Status DeQueue(SqQueue *Q, QElemType *e)
-{
-    if (Q->front == Q->rear)
-    {
-        return ERROR;
-    }
-
-    *e = Q->data[Q->front];
-    Q->front = (Q->front + 1) % MAXSIZE;
-
-    return OK;
-}
-
 int main()
 {
     SqQueue Q;
--- SqQueue/SqQueue.c
+++ SqQueue/SqQueue.c
@@ -25,3 +25,28 @@ int QueueLength(SqQueue Q)
 {
     return (Q.rear - Q.front + MAXSIZE) % MAXSIZE;
 }
+
+Status EnQueue(SqQueue *Q, QElemType e)
+{
+    if ((Q->rear + 1) % MAXSIZE == Q->front)
+    {
+        return ERROR;
+    }
+
+    Q->data[Q->rear] = e;
+    Q->rear = (Q->rear + 1) % MAXSIZE;
+    return OK;
+}
+
+Status DeQueue(SqQueue *Q, QElemType *e)
+{
+    if (Q->front == Q->rear)
+    {
+        return ERROR;
+    }
+
+    *e = Q->data[Q->front];
+    Q->front = (Q->front + 1) % MAXSIZE;
+
+    return OK;
+}
