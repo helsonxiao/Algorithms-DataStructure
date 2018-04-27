b4254cf9e22c2fbf92f440fbee23c3859232f58e
helsonxiao <helsonxiao@gmail.com>
2018-04-27 15:31:41 +0800

    simple search

--- Search/BinarySearch.c
+++ Search/BinarySearch.c
@@ -16,3 +16,23 @@ int BinarySearch (int *a, int n, int key)
 
     return 0;
 }
+
+// 优化查找"均匀分布对象"的速度
+int InterpolationSearch(int *a, int n, int key)
+{
+    int low, high, mid;
+    low = 1;
+    high = n;
+    while (low < high)
+    {
+        mid = low + (high - low) * (key - a[low]) / (a[high] - a[low]);
+        if (key < a[mid])
+            high = mid - 1;
+        else if (key > a[mid])
+            low = mid + 1;
+        else
+            return mid;
+    }
+
+    return 0;
+}
---
+++ Search/FibonacciSearch.c
@@ -0,0 +1,37 @@
+// F={0,1,1,2,3,5,8,13,21,...}
+
+int FibonacciSearch (int *a, int n, int key)
+{
+    int low, high, mid, i, k;
+    low = 1;
+    high = n;
+    k = 0;
+    while (n > F[k] - 1)
+        k++;
+    for (i = n; i < F[k] - 1; i++)
+        a[i] = a[n];
+
+    while (low <= high)
+    {
+        mid = low + F[k-1] - 1;
+        if (key < a[mid])
+        {
+            high = mid -1;
+            k = k - 1;
+        }
+        else if (key > a[mid])
+        {
+            low = mid + 1;
+            k = k - 2;
+        }
+        else
+        {
+            if (mid <= n)
+                return mid;
+            else
+                return n;
+        }
+    }
+
+    return 0;
+}
