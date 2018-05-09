30aceba4c347b0632674f294237e25ec2e7a7255
helsonxiao <helsonxiao@gmail.com>
2018-05-09 12:07:42 +0800

    optimize

--- Sort/InsertSort.c
+++ Sort/InsertSort.c
@@ -3,27 +3,28 @@
 
 void InsertSort (SqList *L)
 {
-    int i, j;
-    for (i = 2; i < L->length; i++)
+    int i, j, tmp;
+    for (i = 1; i < L->length; i++)
         if (L->r[i-1] > L->r[i])
         {
-            L->r[0] = L->r[i];
-            for (j = i - 1; L->r[j] > L->r[0]; j--)
+            tmp = L->r[i];
+            for (j = i - 1; L->r[j] > tmp; j--)
                 L->r[j+1] = L->r[j];
-            L->r[j+1] = L->r[0];
+                
+            L->r[j+1] = tmp;
         }
 }
 
-int main()
-{
-    SqList L;
-    int r[] = {0, 5, 3, 4, 6, 2};
-    int length = 6;
-    L.length = length;
-    int i;
-    for (i = 0; i < L.length; i++)
-        L.r[i] = r[i];
+// int main()
+// {
+//     SqList L = {
+//         {5, 3, 4, 6, 2},
+//         5
+//     };
 
-    InsertSort(&L);
-    return 0;
-}
+//     InsertSort(&L);
+
+//     int i;
+//     for (i = 0; i < L.length; i++)
+//         printf("%d ", L.r[i]);
+// }
